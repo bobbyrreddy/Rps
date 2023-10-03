3eabe6325610bf80d9fd63c415d98233df0ac982
Bharat Reddy <rikred@Bobs-Mac.local>
2023-10-03 23:58:53 +0530

    prepared the js file for the actual code

---
+++ rps.js
@@ -0,0 +1,37 @@
+function getComputerchoice (){
+    //randomly return rock paper or scissors
+    // math.ramdom. divide it by 3 and accordig to 
+    // the result it'll choose rock paper or scissors.
+
+
+
+
+
+    // use console to see if it is returning the expected
+    //result
+}
+
+// write a function which take two pameters 
+// player selection
+// computer selection. 
+
+const pSelection ="rock";
+const cSelection = getComputerchoice();
+
+function roundOfRPS (pSelection, cSelection){
+    // returns a string
+
+    // you lose! Paper beats Rock
+
+    // make pSelection case-insensitive
+
+
+}
+
+console.log(roundOfRPS());
+
+
+// 5 round game .. use the roundOfRPS five times snd 
+// declare the result.
+// Use console.log to dispaly result
+//use prompt to get user input.
